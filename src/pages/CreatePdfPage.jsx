217d66884d545e19b910133700479ee085c964b9
musiienko25 <devmusiienko@gmail.com>
2024-05-18 01:42:13 +0300

    fixed cirillic fonts

--- src/pages/CreatePdfPage.jsx
+++ src/pages/CreatePdfPage.jsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
 import PDFViewer from "./PdfViewerPage";
+import { Font } from "@react-pdf/renderer";
 
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import Input from "../components/Input";
+
+Font.register({
+  family: "Roboto",
+  fonts: [
+    {
+      src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-light-webfont.ttf",
+      fontWeight: 300,
+    },
+    {
+      src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-regular-webfont.ttf",
+      fontWeight: 400,
+    },
+    {
+      src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-medium-webfont.ttf",
+      fontWeight: 500,
+    },
+    {
+      src: "https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/roboto-bold-webfont.ttf",
+      fontWeight: 600,
+    },
+  ],
+});
 
 const CreatePdfPage = () => {
   const [url, setUrl] = useState(null);
